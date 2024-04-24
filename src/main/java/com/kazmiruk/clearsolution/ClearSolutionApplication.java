3651ed509b116362c1e9ffa378fb378107da811d
Dmytro Kazmiruk <dima.kazmiruk.05@gmail.com>
2024-04-24 19:17:55 +0300

    Init commit

---
+++ src/main/java/com/kazmiruk/clearsolution/ClearSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.kazmiruk.clearsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kazmiruk/clearsolution/ClearSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazmiruk.clearsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
